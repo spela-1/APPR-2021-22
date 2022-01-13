798ee07d83e4d4a8cf0812f73966c438ced090c2
Spela Bernardic <a.spela.bernardic@gmail.com>
2022-01-13 18:14:55 +0100

    vizualizacija: g1, g2, g3, g4 in zamljevid obsojeni po obcinah + popravila nekaj tabel

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -18,16 +18,18 @@ sl <- locale("sl", decimal_mark=".", grouping_mark=",")
 #N	za objavo premalo zanesljiva ocena
 
 #----------------------------------------------------------------------------------
+#######izkazalo se je da te tabele ne bomo uporabili, ker je naslednja tabela iz sursa zanimivejša
+
 ## uvoz tabele zločina po vrstah zločina na 100 000 prebivalcev
 ## HTML
 
 
-zlocin1<- readHTMLTable("podatki/zlocin.html")
-zlocin1 <- zlocin1[["NULL"]]
+#zlocin1<- readHTMLTable("podatki/zlocin.html")
+#zlocin1 <- zlocin1[["NULL"]]
 
 # prevedem imena stolpcev
  
-colnames(zlocin1) <-  c("leto", "nameren umor", "poskus umora", "napad", "ugrabitev", "spolno nasilje", "posilstvo", "spolni napad", "rop", "vlom", "vlom v privatno posest", "kraja", "kraja prevoznaga sredstva", "zločin povezan z drogami")
+#colnames(zlocin1) <-  c("leto", "nameren umor", "poskus umora", "napad", "ugrabitev", "spolno nasilje", "posilstvo", "spolni napad", "rop", "vlom", "vlom v privatno posest", "kraja", "kraja prevoznaga sredstva", "zločin povezan z drogami")
 
 #------------------------------------------------------------------------------------
 
@@ -38,7 +40,8 @@ zlocinci  <- read_delim("podatki/zlocinci.csv",
                                      delim=";", col_names=TRUE, col_select=c(-2), na=c("...", 'M', 'N', 'z'), skip=2, locale=locale(decimal_mark= ".", encoding = "Windows-1250"))
 
 # izberem samo glavne kategorije:
-vzorec1 <- "\\d\\.[IVX][IVX]*\\s.*"  
+
+vzorec1 <- "\\d\\.[IVX][IVX]*\\s.*|[:alpha:].*"  
 #stevilka.[IVX][IVX]*poljubnokrat\\s presleden.* nekaj poljubnokrat
 zlocini2 <- unlist(str_extract_all(zlocinci$"KAZNIVO DEJANJE", vzorec1))
 zlocinci <- zlocinci %>% filter(zlocinci$"KAZNIVO DEJANJE" %in% zlocini2)
@@ -50,6 +53,7 @@ zlocinci <- pivot_longer(zlocinci, cols = colnames(zlocinci)[-1],
                            values_to =  'stevilo obsodb')
 
 #razbijem stolpec spol.starost.leto
+
 zlocinci <- zlocinci %>%
   tidyr::extract(
     col = spol.starost.leto,
@@ -64,9 +68,24 @@ zlocinci <- zlocinci %>%
     regex = "^([MoškiŽenske]*)\\s+(.*)$"
   )
 
-# ne zanimajo me podatki po starostnih skupinah
+colnames(zlocinci) <-c("dejanje", "spol", "starost", "leto", "obsojeni")
 
-zlocinci <- zlocinci %>% filter(zlocinci$"starost" == "Starost - SKUPAJ") %>% select(-"starost")
+
+
+vzorcek = "([-]+)([0-9]*)"
+zlocinci$obsojeni = str_replace_all(zlocinci$obsojeni , vzorcek,"0")
+
+#a <- data.frame(zlocinci$starost)
+#a <- data.frame(lapply(a, function(x) {gsub("Starost - SKUPAJ", "skupaj", x)}))
+#zlocinci$starost <- a
+
+
+#summary(zlocinci)
+zlocinci[["spol"]] <- parse_factor(zlocinci[["spol"]], c("Moški", "Ženske"))
+zlocinci$obsojeni <- parse_double(zlocinci$obsojeni)
+zlocinci$leto <- parse_double(zlocinci$leto)
+zlocinci$starost <- parse_factor(zlocinci[["starost"]], levels = c("Starost - SKUPAJ", "do 20 let", "od 21 do 30 let", "od 31 do 40 let", "od 41 do 50 let", "od 51 do 60 let", "nad 60 let"))
+
 
 
 #-----------------------------------------------------------------------------------
@@ -74,20 +93,20 @@ zlocinci <- zlocinci %>% filter(zlocinci$"starost" == "Starost - SKUPAJ") %>% se
 
   
 obsojeni_r <- read_delim("podatki/obsojeni.csv",
-                         delim=";", col_names=TRUE, na="...", skip=2, locale=locale(decimal_mark= ".", encoding = "Windows-1250"))
+                         delim=";", col_names=TRUE, na=c("...", 'M', 'N', 'z'), skip=2, locale=locale(decimal_mark= ".", encoding = "Windows-1250"))
 
 
 
 
 obsojeni_r <- pivot_longer(obsojeni_r, cols = colnames(obsojeni_r)[-1], 
                          names_to = 'leto',
-                         values_to =  'Stevilo obsojenih na 1000 prebivalcev')
+                         values_to =  'obsojeni')
 
 
 #transponiram :
 #obsojeni_r <- pivot_wider(obsojeni_r,
 #                        names_from = 'STATISTIČNA REGIJA',
-#                        values_from = 'Stevilo obsojenih na 1000 prebivalcev')
+#                        values_from = 'obsojeni')
 
 
 # uredim leta:
@@ -96,10 +115,13 @@ vzorec <- "[2][0][0-9]{2}"
 leta <- unlist(str_extract_all(obsojeni_r$leto, vzorec))
 obsojeni_r$leto <- leta
 
+colnames(obsojeni_r)[1] <- "regija"
 
-# ročno izbrišem stolpec tujina, ker je povsod NA
-# obsojeni_r$'Tujina' <- NULL   ko je bila tabela transponirana
 
+#vn zmečemo tujino
+obsojeni_r<-obsojeni_r[!(obsojeni_r$`regija`=="Tujina"),]
+
+obsojeni_r$obsojeni<- as.double(obsojeni_r$obsojeni)
 #--------------------------------------------------------------------------------------
 ## uvoz tabele samoocene splošnega zadovoljstva
 ##(Samoocena splošnega zadovoljstva z življenjem po statističnih regijah, Slovenija, letno POVPREČJE)
@@ -107,15 +129,15 @@ obsojeni_r$leto <- leta
 
 
 zadovoljstvo <- read_delim("podatki/zadovoljstvo.csv",
-                          delim=";", col_names=TRUE, na="-", skip=2, locale=locale(encoding = "Windows-1250"))
+                          delim=";", col_names=TRUE, na=c("...", 'M', 'N', 'z'), skip=2, locale=locale(encoding = "Windows-1250"))
 
 
 
 zadovoljstvo <- pivot_longer(zadovoljstvo, cols = colnames(zadovoljstvo)[-1], 
                            names_to = 'leto',
-                           values_to =  'Samoocene splošnega zadovoljstva')
+                           values_to =  'zadovoljstvo')
 
-
+colnames(zadovoljstvo)[1] <- "regija"
 # uredim leta:
 
 vzorec <- "[2][0][0-9]{2}"
@@ -126,29 +148,28 @@ zadovoljstvo$leto <- leta
 ## uvoz Stopnja tveganja revščine, statistične regije, Slovenija, letno %
 
 revscina <- read_delim("podatki/revscina.csv",
-                      delim=";", col_names=TRUE, col_select=c(-1), na="-", skip=2, locale=locale(encoding = "Windows-1250"))
+                      delim=";", col_names=TRUE, col_select=c(-1), na=c("...", 'M', 'N', 'z'), skip=2, locale=locale(encoding = "Windows-1250"))
     
 
 
 
 revscina <- pivot_longer(revscina, cols = colnames(revscina)[-1], 
                              names_to = 'leto',
-                             values_to =  'Stopnja tveganja revščine')
+                             values_to =  'revscina')
 
-
-#pri tej tabeli so leta že urejena
-
+colnames(revscina)[1] <- "regija"
 #----------------------------------------------------------------------------------------
 ## uvoz Stanovanjske razmere, statistične regije, Slovenija, letno
 
 stanovanje <- read_delim("podatki/stanovanje.csv",
-                        delim=";", col_names=TRUE, na="-", skip=2, locale=locale(encoding = "Windows-1250"))
+                        delim=";", col_names=TRUE, na=c("...", 'M', 'N', 'z'), skip=2, locale=locale(encoding = "Windows-1250"))
 
 
 stanovanje <- pivot_longer(stanovanje, cols = colnames(stanovanje)[-1], 
                              names_to = 'leto',
-                             values_to =  'Stanovanjske razmere')
+                             values_to =  'stanovanje')
 
+colnames(stanovanje)[1] <- "regija"
 
 # uredim leta:
 
@@ -162,28 +183,34 @@ stanovanje$leto <- leta
 ##          združim v skupno tabelo
 
 # v tabeli 'Samoocene splošnega zadovoljstva' je najstarejši podatek iz leta 2012,
-# zato starejše podatke iz drugih dveh tabel izpustim:
-#     tega ubistvu ni treba, ker pri združevanju tabel že sam naredi
+# zato starejše podatke ostalih tabel izpustim:
 
-#revscina <- revscina %>% filter(leto>=2012)
-#stanovanje <- stanovanje %>% filter(leto>=2012)
+obsojeni_r <- obsojeni_r %>% filter(leto>=2012)
+revscina <- revscina %>% filter(leto>=2012)
+stanovanje <- stanovanje %>% filter(leto>=2012)
 
-tabela_r <- obsojeni_r %>% left_join(zadovoljstvo, by=c("STATISTIČNA REGIJA","leto")) %>%
-            left_join(revscina, by=c("STATISTIČNA REGIJA", "leto")) %>% 
-            left_join(stanovanje, by=c("STATISTIČNA REGIJA", "leto"))
+
+tabela_r <- obsojeni_r %>% left_join(zadovoljstvo, by=c("regija","leto")) %>%
+            left_join(revscina, by=c("regija", "leto")) %>% 
+            left_join(stanovanje, by=c("regija", "leto"))
             
+tabela_r$leto<- as.double(tabela_r$leto)
 ##--------------------------------------------------------------------------------------
 ## uvoz tabele obsojenih na 1000 prebivalcev po občinah
 
 
 obsojeni_o <- read_delim("podatki/obsojenio.csv", 
-                         delim=";", col_names=TRUE, na="-", skip=2, locale=locale(decimal_mark= ".", encoding = "Windows-1250"))
+                         delim=";", col_names=TRUE, na=c("...", 'M', 'N', 'z'), skip=2, locale=locale(decimal_mark= ".", encoding = "Windows-1250"))
 
 
 
 obsojeni_o <- pivot_longer(obsojeni_o, cols = colnames(obsojeni_o)[-1], 
                            names_to = 'leto',
-                           values_to =  'Število obsojenih na 1000 prebivalcev')
+                           values_to =  'obsojeni')
+
+obsojeni_o <- data.frame(lapply(obsojeni_o, function(x) {gsub("-", 0, x)}))
+
+colnames(obsojeni_o)[1] <- "obcina"
 
 # uredim leta:
 
@@ -191,15 +218,14 @@ vzorec <- "[2][0][0-9]{2}"
 leta <- unlist(str_extract_all(obsojeni_o$leto, vzorec))
 obsojeni_o$leto <- leta
 
-
-
-
+#vn zmečemo tujino
+obsojeni_o<-obsojeni_o[!(obsojeni_o$`obcina`=="Tujina"),]
 ##--------------------------------------------------------------------------------
 ## uvoz tabele povprečne plače po občinah 
 #(za 2021 je ze narejen stolpec, vendar vrednosti niso označene kot NA -> vn uzemi stolpec)
 
 placa <- read_delim("podatki/placao.csv", 
-                    delim=";", col_names=TRUE, na="-",col_select=c(-1, -"2021"), skip=2, locale=locale(decimal_mark= ".", encoding = "Windows-1250"),
+                    delim=";", col_names=TRUE, na=c("...", 'M', 'N', 'z'),col_select=c(-1, -"2021"), skip=2, locale=locale(decimal_mark= ".", encoding = "Windows-1250"),
                     col_types = cols(
                       .default = col_guess(), 
                       "2020" = col_double()
@@ -207,10 +233,18 @@ placa <- read_delim("podatki/placao.csv",
                     )
 
 
+placa <- data.frame(lapply(placa, function(x) {gsub("-", 0, x)}))
+
+
 placa <- pivot_longer(placa, cols = colnames(placa)[-1], 
                            names_to = 'leto',
-                           values_to =  'Povprecna plača')
+                           values_to =  'placa')
 
+colnames(placa)[1] <- "obcina"
+
+vzorec <- "[2][0][0-9]{2}"
+leta <- unlist(str_extract_all(placa$leto, vzorec))
+placa$leto <- leta
 
 
 ##--------------------------------------------------------------------------------
@@ -221,6 +255,10 @@ placa <- pivot_longer(placa, cols = colnames(placa)[-1],
 
 o <- obsojeni_o %>% filter(leto>=2008)
 
-tabela_o <- o %>% left_join(placa, by=c("OBČINE","leto")) 
+tabela_o <- o %>% left_join(placa, by=c("obcina","leto")) 
 
+tabela_o$leto<- as.double(tabela_o$leto)
+tabela_o$obsojeni<- as.double(tabela_o$obsojeni)
+tabela_o$placa<- as.double(tabela_o$placa)
 ##--------------------------------------------------------------------------------
+
--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,1 +1,102 @@
 # 3. faza: Vizualizacija podatkov
+
+
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+
+source("uvoz/uvoz.r", encoding="UTF-8")
+
+#torej mam tri glavne tabele s katerimi želim risati grafe:
+#zlocinci, tabela_r in tabela_o
+
+
+
+g1 <- tabela_r %>% filter(regija!="SLOVENIJA") %>%
+  ggplot(
+    mapping = aes(x = leto, y = obsojeni, color = regija)
+  ) +
+  ylab("Število obsojenih na 1000 prebivalcev") + 
+  geom_line() + geom_point()
+  
+
+print(g1)
+            
+
+g2 <- tabela_r %>% filter(regija!="SLOVENIJA") %>%
+  ggplot(
+    mapping = aes(x = zadovoljstvo, y = obsojeni, color= revscina , size= stanovanje)
+  ) +
+  ylab("Število obsojenih na 1000 prebivalcev")  +
+  xlab("Samoocena splošnega zadovoljstva z življenjem") + 
+  geom_point(position = position_jitter(width = 0.10))      
+             
+print(g2)
+
+
+
+g3 <- zlocinci %>% filter(dejanje == "KAZNIVO DEJANJE - SKUPAJ") %>%
+  filter(starost != "skupaj") %>%
+  ggplot(
+    mapping = aes(x = leto, y = obsojeni ,  fill = spol)
+  )  +
+  ylab("Število obsojenih") + 
+  geom_col(
+    position = position_dodge()
+  )
+             
+print(g3)
+
+
+g4 <- zlocinci %>% filter(starost != "Starost - SKUPAJ") %>%
+  group_by(starost) %>%
+  ggplot(
+    mapping = aes(x = leto, y = obsojeni ,  fill = starost)
+  )  +
+  ylab("Število obsojenih") + 
+  geom_col()
+
+print(g4)
+
+########################################## ZEMLJEVIDI #########################################################
+tabela <- tabela_o %>% filter(leto =="2013") %>% filter(obcina != "SLOVENIJA" ) 
+
+library(sp)
+library(rgdal)
+library(rgeos)
+library(raster)
+library(tidyverse)
+library(tmap)
+
+source("lib/uvozi.zemljevid.r")
+obcine <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip", "OB",
+                          pot.zemljevida="OB", encoding="Windows-1250")
+
+tm_shape(obcine) + tm_polygons("OB_UIME") + tm_legend(show=FALSE)
+obcine$OB_UIME <- factor(obcine$OB_UIME)
+
+lvls <- levels(obcine$OB_UIME)
+obsojeni.zemljevid <- unique(tabela$obcina) %>% sort()
+
+razlicni <- lvls!= obsojeni.zemljevid
+
+primerjava <- data.frame(obcina.zemljevid = parse_character(lvls),
+                         obcina.obsojeni = obsojeni.zemljevid)[razlicni,]
+
+obsojeni.na.zemljevidu <- tabela%>% 
+  left_join(primerjava, by = c("obcina" = "obcina.obsojeni")) %>%
+  mutate(obcina = ifelse(is.na(obcina.zemljevid), obcina, obcina.zemljevid) %>% factor()) %>%
+  dplyr::select(-obcina.zemljevid)
+
+obcine.obsojeni.zemljevid <- merge(obcine, obsojeni.na.zemljevidu,
+                                by.x = "OB_UIME", by.y = "obcina")
+
+tm_shape(obcine.obsojeni.zemljevid) +
+  tm_polygons("obsojeni", popup.vars = c("Število obsojenih na 1000 preb: " = "obsojeni")) + tm_legend(show=FALSE)
+tmap_mode("view") 
+
+
+
+
+
+
